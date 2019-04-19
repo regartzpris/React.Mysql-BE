341a137385a6d0d2a4bc0f4ae4ac90ed4f348615
Priskila Ritonga <ritongapriskila@gmail.com>
2019-04-19 08:59:47 +0700

    add users

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+const port = 2010
+
+
+app.use(express.json())
+const conn = mysql.createConnection({
+    user: 'kila',
+    password: 'Mysql123',
+    host: 'localhost',
+    database: 'mysqlexpress',
+    port: '3306'
+})
+
+
+app.post('/users', (req, res) => {
+    const { name, age, password, email } = req.body
+    var sql = `INSERT INTO users (name, age,password,email) VALUES ('${name}', ${age},'${password}','${email}');`
+    var sql2 = `SELECT * FROM users;`
+
+    conn.query(sql, (err, result) => {
+        if (err) { throw err }
+
+        conn.query(sql2, (err, result) => {
+            if (err) { throw err }
+
+            res.send(result)
+        })
+    })
+})
+
+
+app.listen(port, () => {
+    console.log('Api Running at', port);
+})
